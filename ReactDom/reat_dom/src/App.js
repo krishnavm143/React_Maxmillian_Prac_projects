4da2bff5627490c02f091a52a8832a22418f9ca1
krishna <krishnavm143@gmail.com>
2023-04-14 17:38:22 +0530

    commmit

---
+++ ReactDom/reat_dom/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import Button from './components/UI/Button/Button'
+function App() {
+const [showPara,setShowPara]=  useState(false)
+ const toggleParagraphHandler =( ) =>{
+  setShowPara(prevShowPara => !prevShowPara)
+ }
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showPara && <p>This is new</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
