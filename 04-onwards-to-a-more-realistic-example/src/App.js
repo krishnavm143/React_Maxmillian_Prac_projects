53dc6c6ae8de37bc7f39288dffa0b7acf11d3ee7
krishna <krishnavm143@gmail.com>
2023-04-19 14:54:45 +0530

    worked on custom hooks

---
+++ 04-onwards-to-a-more-realistic-example/src/App.js
@@ -0,0 +1,77 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  
+ 
+ const httpData = useHttp()
+
+  const {isLoading,error,sendRequest :fetchTasks}=httpData
+
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://fetch-app-7d637-default-rtdb.firebaseio.com/tasks.json'
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    const tranformedTask =  taskObj => {
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    }
+    fetchTasks({
+      url: 'https://fetch-app-7d637-default-rtdb.firebaseio.com/tasks.json'
+    },
+      tranformedTask
+      
+      );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
